f5d631a487308ab0270126bce5a42e54d7e21fd8
Fabrizio Togni <fabrizio.togni@studioaquattro.local>
2025-04-26 08:15:20 +0200

    Aggiunto controller apertura varchi openAccesso

--- routes/accessi.js
+++ routes/accessi.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { configuraAccesso } from '../controllers/accessiConfiguraController.js';
 import { abilitaAccesso } from '../controllers/accessiAbilitaController.js';
+import { openAccesso } from '../controllers/accessiOpenController.js';
 
 const router = express.Router();
 
@@ -9,5 +10,8 @@ router.post('/configura', configuraAccesso);
 
 // Route per abilitare un utente ad aprire un varco
 router.post('/abilita', abilitaAccesso);
+
+// Route per aprire un varco
+router.post('/open', openAccesso);
 
 export default router;
